531bbc280f9d47db94a78e302266177510fa118e
caprosset <capucinerosset@gmail.com>
2019-12-04 15:46:50 +0100

    Iteration 1-2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import Foodbox from './components/FoodBox';
+import shortid from 'shortid';
+
 
 class App extends Component {
+
+  state = {
+    foods: foods
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +19,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        
+        
+        {
+          this.state.foods.map( (food) => {
+            return <Foodbox key={shortid.generate()} {...food} />
+          })
+        }
       </div>
     );
   }
